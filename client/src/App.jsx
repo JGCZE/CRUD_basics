5ea2b6f76dd6bde84188277130c475637aebc2ba
Jan Gross <jan.gross.cze@gmail.com>
2023-08-11 13:47:38 +0200

    add css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,23 @@ import React from 'react'
 const App = () => {
   return (
     <div className='App'>
-      hello
+      <div className='information'>
+        <label>Name:</label>
+        <input type="text" />
+
+        <label>Age:</label>
+        <input type="number" />
+
+        <label>Country:</label>
+        <input type="text" />
+
+        <label>Position:</label>
+        <input type="text" />
+
+        <label>Wage:</label>
+        <input type="number" />
+        <button>Add Employee</button>
+      </div>
     </div>
   ) 
 }
